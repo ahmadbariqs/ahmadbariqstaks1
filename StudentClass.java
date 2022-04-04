4347cfe4546a128f1f39c5f6cded7ce168530ef7
ahmadbariqs <101858455+ahmadbariqs@users.noreply.github.com>
2022-04-04 12:01:44 +0700

    Add files via upload

---
+++ StudentClass.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author AHMAD BARIQ
+ */
+public class StudentClass {
+    public static void main(String []args) {
+       String name, major;
+       int age;
+       
+       name = "Ahmad Bariq";
+       major = "Information Technology";
+       age = 18;
+       
+       System.out.println("Name chosen is  :" + name);
+       System.out.println("Major chosen is :" + major);
+       System.out.println("Student Age is  :" + age);
+         
+     }
+
+}
